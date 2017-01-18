b058fdeda29af7824811fd662f2f86f00e78ae4b
Chanel FU <cfu@e1z3r7p25.42.us.org>
2017-01-17 19:27:42 -0800

    fillit

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: cfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/13 15:12:23 by cfu               #+#    #+#             */
-/*   Updated: 2017/01/13 19:03:31 by cfu              ###   ########.fr       */
+/*   Updated: 2017/01/16 21:23:41 by cfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,28 @@
 
 #define BUFF_SIZE 21
 
-#include "libft/libft.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
 
-static char **v_tets = {"####", "#....#....#....#", "#....#....#....##",
+static char *v_tets[] = {"####", "#....#....#....#", "#....#....#....##",
 						"##...#....#", "##....#....#", "#....#....#...##",
 						"###..#", "###....#", "#....##...#", "#...##....#",
 						"###...#", "#...###", "##....##", "##..##", 
 						"#....##....#", "#...##...#", "##...##"};
 
-int					ft_getfile(int fd);
-
-static void 		ft_plcbrs(char *str);
+void 				ft_repchar(char *str, char o, char n, int max);
 char				*ft_gettet(int fd);
+char				*ft_trim_tet(char *str, char c);
+char				*ft_strnew(size_t size);
+size_t				ft_strlen(const char *s);
+void				ft_putchar(char c);
+void				ft_putstr(char const *s);
+void				*ft_memmove(void *dst, const void *src, size_t len);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strrchr(const char *s, int c);
+void				ft_bzero(void *s, size_t n);
+void				*ft_memset(void *b, int c, size_t len);
 
 #endif
