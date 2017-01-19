4724bf6d7d44b1d2205bce3f5721efc29edbdbd1
Chanel FU <cfu@e1z2r8p27.42.us.org>
2017-01-18 18:38:58 -0800

    wip

--- ft_fillit.c
+++ ft_fillit.c
@@ -1,24 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_fillit.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/16 17:11:31 by cfu               #+#    #+#             */
-/*   Updated: 2017/01/18 17:37:22 by cfu              ###   ########.fr       */
+/*   Created: 2017/01/18 17:18:05 by cfu               #+#    #+#             */
+/*   Updated: 2017/01/18 17:37:40 by cfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		main(int argc, char **argv)
+void	ft_fillit(int fd)
 {
-	int		fd;
+	char	*res;
+	int		valid;
 
-	if (argc < 1)
-		return (-1);
-	fd = open(argv[1], O_RDONLY);
-	ft_fillit(fd);
-	return (0);
+	res = ft_gettet(fd);
+	valid = 0;
+	valid = ft_validate_tets(res);
+	ft_pound_to_letter(res);
+	ft_putstr(res);
+	ft_putchar('\n');
+	ft_putnbr(valid);
 }
--- ft_pound_to_letter.c
+++ ft_pound_to_letter.c
@@ -1,24 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_pound_to_letter.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cfu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/16 17:11:31 by cfu               #+#    #+#             */
-/*   Updated: 2017/01/18 17:37:22 by cfu              ###   ########.fr       */
+/*   Created: 2017/01/18 16:45:57 by cfu               #+#    #+#             */
+/*   Updated: 2017/01/18 16:53:10 by cfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		main(int argc, char **argv)
+void		ft_pound_to_letter(char *str)
 {
-	int		fd;
-
-	if (argc < 1)
-		return (-1);
-	fd = open(argv[1], O_RDONLY);
-	ft_fillit(fd);
-	return (0);
+	int			i;
+	static int	al;
+	int			len;
+	
+	if (!al)
+		al = 0;
+	i = 0;
+	len = ft_strlen(str);
+	while (i < len)
+	{
+		if (str[i] == '#')
+			str[i] = 'A' + al;
+		i++;
+	}
+	al++;
 }
